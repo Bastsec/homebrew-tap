57b2a6f402f75ec819d24554da92c85b7c3f5e89
kifaru-release <release@kifaru.local>
2026-07-06 19:02:01 +0000

    Update to v1.0.135

--- kifaru.rb
+++ kifaru.rb
@@ -4,19 +4,19 @@
 class Kifaru < Formula
   desc "Autonomous security testing agent for the terminal."
   homepage "https://github.com/Bastsec/kifaru"
-  version "1.0.134"
+  version "1.0.135"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/Bastsec/kifaru/releases/download/v1.0.134/kifaru-darwin-x64.zip"
-      sha256 "f36d54ccb7c945bd19324c05d907fb8e12dbac543302d88082ce87871931498b"
+      url "https://github.com/Bastsec/kifaru/releases/download/v1.0.135/kifaru-darwin-x64.zip"
+      sha256 "b8a9e73efd4d4cec1db8cd7a36e73a3eb91d8cd67ba67a3226617494875ce1b9"
       def install
         bin.install "kifaru"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/Bastsec/kifaru/releases/download/v1.0.134/kifaru-darwin-arm64.zip"
-      sha256 "20e3a29c8ce1a3c4493ae9b7e64196d14b2fb426aefdaea18caec645d6058423"
+      url "https://github.com/Bastsec/kifaru/releases/download/v1.0.135/kifaru-darwin-arm64.zip"
+      sha256 "2cbc59ed62ff853569d051516ae1b1174d27085b544ec713a206e9353563e6fb"
       def install
         bin.install "kifaru"
       end
@@ -25,15 +25,15 @@ class Kifaru < Formula
 
   on_linux do
     if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
-      url "https://github.com/Bastsec/kifaru/releases/download/v1.0.134/kifaru-linux-x64.tar.gz"
-      sha256 "c1b6b6c657762adb14a9d4b883edac3f7517ade4d582507e2ee1c4e3cea798a2"
+      url "https://github.com/Bastsec/kifaru/releases/download/v1.0.135/kifaru-linux-x64.tar.gz"
+      sha256 "54f1d5a36114afcc360b5d2f100730dbaf35bf8106fbedc82d2a25a700796d01"
       def install
         bin.install "kifaru"
       end
     end
     if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
-      url "https://github.com/Bastsec/kifaru/releases/download/v1.0.134/kifaru-linux-arm64.tar.gz"
-      sha256 "fca6c2b83637322cc00a4ee7e99fd37281fffc71216a482a90ee881807ce629f"
+      url "https://github.com/Bastsec/kifaru/releases/download/v1.0.135/kifaru-linux-arm64.tar.gz"
+      sha256 "fb6daf4b9a1f365efd6f60f951a0a57b07d651412f45561947505f9d61d6439f"
       def install
         bin.install "kifaru"
       end
